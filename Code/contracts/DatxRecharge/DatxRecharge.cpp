96cc528b89f5eab1d9a76d657dcb8a7ba2ec6f37
wujieyu <jieyu.wu@dotcunited.com>
2018-10-09 10:21:45 +0800

    update DatxRecharge

--- Code/contracts/DatxRecharge/DatxRecharge.cpp
+++ Code/contracts/DatxRecharge/DatxRecharge.cpp
@@ -1,55 +1,239 @@
 #include "DatxRecharge.hpp"
 #include <DatxioLib/multi_index.hpp>
+#include <fc/time.hpp>
+#include <fc/string.hpp>
+#include <cstring>
+#include <DatxioLib/crypto.h>
 namespace datxio
 {
 /// @abi action
-
-void recharge::charge(transaction_id_type hash,string from,string to,int64_t blocknum,string quantity,string category,string memo)
+void recharge:recorduser(account_name datxaddress,string address,account_namebpname)
 {
+     require_auth(bpname);
+        account_name producers[21]; 
+        uint32_t bytes_populated = get_active_producers(producers, sizeof(account_name)*21); 
+        bool Isproducer = false; 
+        for (int i = 0; i < sizeof(producers)/sizeof(account_name) ;i++){ 
+             if(producers[i] == bpname) 
+             Isproducer = true; 
+        } 
+        datxio_assert(Isproducer, "this func can only be called by producers");
 
-   // define the table
-        require_auth(_self);
-        records records_table(_self,_self);
-         auto idx = records_table.template get_index<N(hash)>();
-         auto itr = idx.find( record::get_hash(hash) );
-        datxio_assert(itr==idx.end(), "hash for transaction already exists");
+    // define the table 
+        users users_table(_self,_self);
+         auto idx = records_table.template get_index<N(address)>();
+         auto itr = idx.find( user::get_hash(address) );
+        datxio_assert(itr==idx.end(), "This address is already exists");
+        users_table.emplace(_self, [&](auto &u) {
+            u.userid= users_table.available_primary_key();
+            u.datxaddress=datxaddress;
+            u.address=address;
+        });
+}
 
-        records_table.emplace(_self, [&](auto &s) {
-            s.trxid= records_table.available_primary_key();
-            s.hash= hash;
+void recharge::charge(account_name bpname,transaction_id_type hash,string from,string to,int64_t blocknum,string quantity,string category,string memo)
+{
+        require_auth(bpname);
+        string txHash = to_hex((const char*)(&hash), sizeof(hash));
+    //check the caller is producer
+        account_name producers[21]; 
+        uint32_t bytes_populated = get_active_producers(producers, sizeof(account_name)*21); 
+        bool Isproducer = false; 
+        for (int i = 0; i < sizeof(producers)/sizeof(account_name) ;i++){ 
+             if(producers[i] == bpname) 
+             Isproducer = true; 
+        } 
+        datxio_assert(Isproducer, "this func can only be called by producers");
+        
+       
+    //get all expired transactions and remove to expirdrecord
+        expired_trx();
+    //check the countrecord whether exist
+        
+        countrecords count_table(_self,_self);
+        successrecords success_table(_self,_self);
+        expiredrecords expired_table(_self,_self);
+         records records_table(_self,_self);
+        
+        
+
+//(3)check the hash whether exits
+    // caculate hash for trx data
+        string str=txhash+from+to+to_string(blocknum)+quantity+category+memo;
+        char *data=(char*)str.c_str();
+        checksum calc_hash;
+        sha256( data, length, &calc_hash );
+    //check if the hash alerday success
+        auto idx2 = success_table.template get_index<N(data)>();
+        auto itr2 = idx2.find(record::by_trxid(calc_hash) );
+        datxio_assert(itr2==idx2.end(), "This transaction is already success");
+
+    //check the data hash whether equal
+        auto idx = records_table.template get_index<N(data)>();
+        auto itr = idx.find(record::by_data(calc_hash));
+        if(itr==idx.end()){
+
+
+    //check producer whether more than 15
+        bool bpexist=false;
+        for( auto it = itr->producers.begin(); it !=itr->producers.end(); ++it )
+        {
+            if(itr->producers[i] == bpname)
+            {
+                bpexist=true;
+            }
+            datxio_assert(bpexit, "This producer already initiated a request for this transaction!");
+        }
+        records_table.modify(itr,0, [&](auto &s) 
+        {
+               s.producers=s.producers.push_back(bpname)
+
+        }
+
+//(6)transanfer to datx account
+        
+         // you should add DatxRecharge permission first, and then can send action DatxToken contract.
+       
+        if(sizeof(itr.producers>=14))
+        {
+
+            assert quant{to_int64(quantity),S(4,category)}
+            if(category=="BTC"||category=="ETH"){
+                users user_table(_self,_self);
+                auto index = users_table.template get_index<N(address)>();
+                auto it = idx.find( user::get_hash(address) );
+                bool address=false;
+                if(it==index.end())
+                {
+                    address=true;
+                }
+                datxio_assert(address, "This address isn't already exists");
+                if(address==true)
+                {
+                    
+                    if(category=="BTC")
+                    {
+                        action(
+                            permission_level{N(datxio.dbtc), N(active) },
+                            N(datxio.dbtc), N(transfer),
+                            std::make_tuple(N(datxio.dbtc), it, quant,memo)
+                        ).send();
+                    }else{
+                         action(
+                            permission_level{N(datxio.deth), N(active) },
+                            N(datxio.deth), N(transfer),
+                            std::make_tuple(N(datxio.deth), it, quant,memo)
+                        ).send();
+                    }
+                }
+            }else{
+                action(
+                            permission_level{N(datxio.deos), N(active) },
+                            N(datxio.deos), N(transfer),
+                            std::make_tuple(N(datxio.deos), N(memo), quant,memo)
+                        ).send();
+            }
+        }
+
+        success_table.emplace(_self, [&](auto &s) 
+        {
+            s.id= records_table.available_primary_key();
+            s.trxid= hash;
             s.from = from;
             s.to = to;
             s.blocknum= blocknum;
             s.quantity=quantity;
             s.category=category;
             s.memo = memo;
-        });
+            s.data=calc_hash; 
+        }
+        
+        
+        records_table.erase(itr);//remove the transaction in the record_table
+        auto in = expired_table.template get_index<N(data)>();
+        auto i = idx.find( expiredrecord::by_data(calc_hash) );
+        if(in==i)
+        {
+            expired_table.erase(i);
+        }
+        }else{
+            vector<account_name> producers_name;
+            producers_name.push_back(bpname);
+            records_table.emplace(_self, [&](auto &s) {
+                s.id= records_table.available_primary_key();
+                s.trxid= hash;
+                s.from = from;
+                s.to = to;
+                s.blocknum= blocknum;
+                s.quantity=quantity;
+                s.category=category;
+                s.memo = memo;
+                s.data=calc_hash; 
+                s.time=time_point_sec(now()); //seconds
+                memcpy(s.producers,producers_name,sizeof(producers_name));
+            });
+
+        }
 
 }
 
-void recharge::transtoken(transaction_id_type hash,account_name from,account_name to,asset quantity,string memo)
+//get the expired transaction from record
+void recharge::expired_trx()
 {
-     
-    require_auth(_self);
-        transactions trans_table(_self,_self);
-        auto idx = trans_table.template get_index<N(hash)>();
-        auto itr = idx.find( transaction::get_hash(hash) );
-        datxio_assert(itr==idx.end(), "hash for transaction already exists");
-        trans_table.emplace(_self, [&](auto &s) {
-            s.trxid= trans_table.available_primary_key();
-            s.hash= hash;
-            s.from = from;
-            s.to = to;
-            s.memo = memo;
-        });
-        // you should add DatxRecharge permission first, and then can send action DatxToken contract.
-        action(
-            permission_level{from, N(active) },
-           from, N(transfer),
-            std::make_tuple(from, to, quantity,memo)
-        ).send();
+    records trans_table(_self,_self);
+    auto idx = trans_table.template get_index<N(start_time)>();
+    for ( auto it = idx.cbegin(); it != idx.cend();)
+    {
+        if(current_time() - it->start_time < 5*60*1000000)
+        {
+            break;
+        }
+        if(it->producers.size() < 15)
+        {
+            countrecords count_table(_self,_self);
+            auto id =count_table.template get_index<N(data)>();
+            auto i = id.find( expiredrecord::by_data(it->data) );
+            if(i==id.end()&&i->count >=3)
+            {  
+                break;
+            }else
+            {
+                if(i==id.end()&&i->count <3)
+                {
+                    count_table.modify(itr, get_self(), [&](auto& c)
+                    {
+                        c.count=i->count+1;
+                    });
+                }else
+                {
+                    count_table.emplace(_self, [&](auto &co){
+                        co.id=count_table.available_primary_key();
+                        co.data=it->data;
+                        co.count=1;
+                    });
+                }
+                expiredrecords expire_table(_self,_self);
+                expire_table.emplace(_self, [&](auto &e) {
+                    e.id= success_table.available_primary_key();
+                    e.trxid=it->trxid;
+                    e.from=it->from;
+                    e.blocknum=it->blocknum;
+                    e.quantity=it->quantity;
+                    e.category=it->category;
+                    e.memo=it->memo;
+                    e.data=it->data;        
+                });
+            }
+            trans_table.erase(it);
+        }
+       
+        else
+        {
+             ++it; 
+        }
+    }
 }
-
+   
 } // namespace Datxio
 
 
@@ -72,4 +256,4 @@ extern "C" { \
    } \
 }
 
-DATXIO_ABI_EX(datxio::recharge,(charge)(transtoken))
+DATXIO_ABI_EX(datxio::recharge,(recorduser)(charge))
--- Code/contracts/DatxRecharge/DatxRecharge.hpp
+++ Code/contracts/DatxRecharge/DatxRecharge.hpp
@@ -9,11 +9,23 @@ namespace datxio
 using std::string;
 class recharge : public contract
 {
+  private:
+    void expired_trx();
   public:
     recharge(account_name self) : contract(self) {}
+    static key256 get_hash(const checksum256& hash) {
+            const uint64_t *p64 = reinterpret_cast<const uint64_t *>(&hash);
+            return key256::make_from_word_sequence<uint64_t>(p64[0], p64[1], p64[2], p64[3]);
+         }
+    ///@abi action
+    void recorduser(account_name datxaddress,
+                    transaction_id_type address,
+                    account_name bpname);
+
 
     /// @abi action
     void charge(transaction_id_type hash,
+                account_name bpname,
                 string from,
                 string to,
                 int64_t blocknum,
@@ -21,56 +33,111 @@ class recharge : public contract
                 string category,
                 string memo);
 
-    /// @abi action
-    void transtoken(transaction_id_type hash,
-            account_name from,
-            account_name to,
-            asset quantity,
-            string memo);
+     /// @abi table
+     struct user
+     {
+        uint64_t userid; //primary key
+        account_name datxaddress; 
+        transaction_id_type address;
+        uint64_t primary_key() const { return userid; }
+        key256 by_address() const {return get_hash(address);}
+        account_name get_datxaddress() const {return datxaddress;}
+        DATXLIB_SERIALIZE(user, (userid)(datxaddress)(address))
+     }
 
-  public:
+    typedef multi_index<N(user), user,
+                        indexed_by<N(address), const_mem_fun<user, key256, &user::by_address>>,
+                        indexed_by<N(datxaddress), const_mem_fun<user, account_name, &user::get_datxaddress>>> users;
+
      /// @abi table
     struct record
     {
-        uint64_t trxid; //primary key
-        transaction_id_type hash;
+        uint64_t id; //primary key
+        transaction_id_type trxid;
         string from;
         string to;
         int64_t blocknum;
         string quantity;
         string category;
         string memo;
-
+        checksum256 data;
+        time start_time;
+        vector<account_name> producers;
         uint64_t primary_key() const { return trxid; }
-        key256 by_hash() const {return get_hash(hash);}
-        static key256 get_hash(const checksum256& hash) {
-            const uint64_t *p64 = reinterpret_cast<const uint64_t *>(&hash);
-            return key256::make_from_word_sequence<uint64_t>(p64[0], p64[1], p64[2], p64[3]);
-         }
-        DATXLIB_SERIALIZE(record, (trxid)(hash)(from)(to)(quantity)(blocknum)(category)(memo))
+        key256 by_data() const {return get_hash(data);}
+        account_name get_bpname() const { return bpname;}
+        time by_start_time() const {return start_time;}
+
+        DATXLIB_SERIALIZE(record, (id)(trxid)(from)(to)(quantity)(blocknum)(category)(memo)(data)(start_time)(producers))
     };
+
+    typedef multi_index<N(record), record,
+                        indexed_by<N(data), const_mem_fun<record, key256, &record::by_hash>>
+                        ,indexed_by<N(start_time),const_mem_fun<record,time,&record::by_start_time>>> records;
+
     /// @abi table
-    struct transaction
-    {
-        uint64_t trxid; //primary key
-        transaction_id_type hash;
-        account_name from;
-        account_name to;
-        asset quantity;
-        string memo;
+        struct countrecord
+        {
+            uint64_t id; //primary key
+            checksum256 data;
+            int8_t count;
+            uint64_t primary_key() const { return id; }
+            key256 by_data() const {return get_hash(data);
+            DATXLIB_SERIALIZE(countrecord, (id)(data)(count))
+        };
 
-        uint64_t primary_key() const { return trxid; }
-        key256 by_hash() const {return get_hash(hash);}
-        static key256 get_hash(const checksum256& hash) {
-            const uint64_t *p64 = reinterpret_cast<const uint64_t *>(&hash);
-            return key256::make_from_word_sequence<uint64_t>(p64[0], p64[1], p64[2], p64[3]);
-         }
-        DATXLIB_SERIALIZE(transaction, (trxid)(hash)(from)(to)(quantity)(memo))
+        typedef multi_index<N(countrecord),countrecord,
+                            indexed_by<N(data), const_mem_fun<countrecord, key256, &countrecord::by_hash>>> countrecords;
+
+
+
+    ///@abi table
+    struct expiredrecord
+    {
+        uint64_t id; //primary key
+        transaction_id_type trxid;
+        string from;
+        string to;
+        int64_t blocknum;
+        string quantity;
+        string category;
+        string memo;
+        checksum256 data;
+        uint64_t primary_key() const { return id; }
+        key256 by_data() const {return get_hash(data);}
+        account_name get_bpname() const { return bpname;}
+        DATXLIB_SERIALIZE(expiredrecord, (id)(trxid)(from)(to)(quantity)(blocknum)(category)(memo))
     };
-    typedef multi_index<N(record), record,indexed_by<N(hash), const_mem_fun<record, key256, &record::by_hash>>> records;
-    typedef multi_index<N(transaction), transaction,indexed_by<N(hash), const_mem_fun<transaction, key256, &transaction::by_hash>>> transactions;
+
+    typedef multi_index<N(expiredrecord), expiredrecord,
+                        indexed_by<N(data), const_mem_fun<expiredrecord, key256, &expiredrecord::by_hash>>> expiredrecords;
+
+
+    ///@abi table
+     struct successrecord
+    {
+        uint64_t id; //primary key
+        transaction_id_type trxid;
+        string from;
+        string to;
+        int64_t blocknum;
+        string quantity;
+        string category;
+        string memo;
+        checksum256 data;
+        time_point_sec time;
+        std::vector<account_name> producers;
+        uint64_t primary_key() const { return id; }
+        key256 by_data() const {return get_hash(data);}
+        DATXLIB_SERIALIZE(successrecord, (id)(trxid)(from)(to)(quantity)(blocknum)(category)(memo)(data)(time)(producers))
+    };
+
+    typedef multi_index<N(successrecord), successrecord,
+                        indexed_by<N(data), const_mem_fun<successrecord, key256, &successrecord::by_hash>>> successrecords;
 
 
 };
+
+
 
 } // namespace Datxio
